99def467651336c79f3de176822af074207960d9
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-04-10 07:24:03 +0530

    Insertion, Updation and Get User in Prisma

---
+++ Prisma/src/index.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+    const res = await prisma.user.create({
+        data: {
+            password,
+            email:username,
+            firstName,
+            lastName
+        },
+        select:{
+            id:true,
+            password:true,
+            firstName:true
+        }
+    })
+  console.log(res);
+}
+// insertUser("bhavnagarg@gmail.com", "paswordd", "Bhaasvna", "Gssarg")
+
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+    const res = await prisma.user.update({
+        where: {
+            email: username
+        },
+        data:{
+            firstName,
+            lastName
+        }
+    })
+    console.log(res);
+}
+// updateUser('bhavnagarg@gmail.com',{
+//     firstName: 'Billo',
+//     lastName: 'Bhavna'
+// })
+
+async function getUser(username: string) {
+  const user = await prisma.user.findFirst({
+    where: {
+        email: username
+    }
+  })
+  console.log(user);
+}
+
+getUser("bhavnagarg@gmail.com");
