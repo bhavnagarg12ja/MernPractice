cf6ee3f96676daf0020ff455a49c1ae9341d9c94
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-04-25 08:11:53 +0530

    TypeScript Apis

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -36,3 +36,86 @@ const user : Readonly<User> = {
     name: 'John',
     age: 20
 }
+//withput record
+interface Userr {
+    id: string;
+    name: string;
+  }
+  
+  type Userss = { [key: string]: Userr };
+  
+  const userss: Userss = {
+    'abc123': { id: 'abc123', name: 'John Doe' },
+    'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+  };
+//record
+interface User3 {
+    id: string;
+    name: string;
+  }
+  
+  type Users = Record<string, User3>;
+  
+  const users: Users = {
+    'abc123': { id: 'abc123', name: 'John Doe' },
+    'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+  };
+  
+  console.log(users['abc123']); // Output: { id: 'abc123', name: 'John Doe' }
+
+//map
+interface User4 {
+    id: string;
+    name: string;
+  }
+  
+  // Initialize an empty Map
+  const usersMap = new Map<string, User4>();
+  
+  // Add users to the map using .set
+  usersMap.set('abc123', { id: 'abc123', name: 'John Doe' });
+  usersMap.set('xyz789', { id: 'xyz789', name: 'Jane Doe' });
+  
+  // Accessing a value using .get
+  console.log(usersMap.get('abc123')); // Output: { id: 'abc123', name: 'John Doe' }
+
+
+//exclude
+type EventType = 'click' | 'scroll' | 'mousemove';
+type ExcludeEvent = Exclude<EventType, 'scroll'>; // 'click' | 'mousemove'
+
+const handleEvent = (event: ExcludeEvent) => {
+  console.log(`Handling event: ${event}`);
+};
+
+handleEvent('click'); // OK
+
+// Type Inference Zod
+import { z } from 'zod';
+import express from "express";
+
+const app = express();
+
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
+type FinalUserSchema = z.infer<typeof userProfileSchema>;
+
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody: FinalUserSchema = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return
+  }
+  // update database here
+  res.json({
+    message: "User updated"
+  })
+});
+
+app.listen(3000);
