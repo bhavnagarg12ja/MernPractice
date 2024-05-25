fcd8773ddbffc9673a023ae8d42d3b83e6b95867
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-05-25 19:45:15 +0530

    backend practice

--- nextjs_backend_practice/app/page.tsx
+++ nextjs_backend_practice/app/page.tsx
@@ -1,13 +1,27 @@
 import axios from "axios";
 async function getUserDetails() {
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
-	return response.data;
+  try {
+    const response = await axios.get("http://localhost:3000/api/user")
+	  return response.data;
+  }  catch(e) {
+    console.log(e);
+  }
 }
 
 export default async function Home() {
+  const userData = await getUserDetails();
   return (
-    <div>
-      hi there
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
+
